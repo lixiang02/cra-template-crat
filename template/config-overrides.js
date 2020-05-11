6f645fbea019740f8cce728eb5c9043e0472f7e4
李响 <>
2020-05-11 17:19:14 +0800

    feat: 0.0.9

--- template/config-overrides.js
+++ template/config-overrides.js
@@ -4,7 +4,25 @@ const {
   customEntryConfig
 } = require("mcf-cra")
 
+const treeShaking = () => config => {
+  const isProdMode = process.env.NODE_ENV === 'production'
+  if(isProdMode && process.env.MODE !== 'cjs'){
+    fixBabelImports('antd', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,//自动打包相关的样式 默认为 style:'css'
+    })(config),
+    fixBabelImports('@mcf/component', {
+      libraryName: '@mcf/components',
+      libraryDirectory: 'lib',
+      camel2DashComponentName: false
+    })(config)
+  }
+  return config
+}
+
 module.exports = override(
   customBuildConfig(),
-  customEntryConfig()
+  customEntryConfig(),
+  treeShaking()
 )
