7e56268f7a9d903e1cff3b13ee204a4f4541545b
李响 <>
2020-05-18 15:25:24 +0800

    feat: 0.0.13

--- template/.huskyrc.js
+++ template/.huskyrc.js
@@ -2,6 +2,8 @@ module.exports = {
   hooks: {
     'commit-msg': 'commitlint -e $HUSKY_GIT_PARAMS',
     'pre-commit': 'pretty-quick --staged && eslint --max-warnings=0 ./src',
-    'pre-push': 'yarn test --watchAll=false'
+    'post-commit': 'conventional-changelog -p angular -i CHANGELOG.md -s -r 0',
+    'pre-push':
+      'git add CHANGELOG.md && git commit -m "docs: add CHANGELOG.md" && yarn test --watchAll=false '
   }
 };
